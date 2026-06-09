3e556887152467dc3c3553af3de9d538b4906aac
omanjyuu <c0a2516828@edu.teu.ac.jp>
2026-06-09 16:33:13 +0900

    演習2:move_ipメソッドを一回だけにする

--- pygame_image.py
+++ pygame_image.py
@@ -22,17 +22,21 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        kk_rct.move_ip(-1,0)
+        mv_x = -1
+        mv_y = 0
 
         key_lst = pg.key.get_pressed() # 全キーの押下状態を取得
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip(0, -1)
+            mv_y += -1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0, 1)
+            mv_y += 1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
+            mv_x += -1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(2, 0)
+            mv_x += 2
+        
+        kk_rct.move_ip(mv_x, mv_y)
+
 
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
