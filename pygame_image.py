1f2d88531677884bf1f8b747dcfa51bd2a938565
omanjyuu <c0a2516828@edu.teu.ac.jp>
2026-06-09 16:13:00 +0900

    練習10：押下キーに応じてこうかとんが移動するようにせよ

--- pygame_image.py
+++ pygame_image.py
@@ -14,16 +14,30 @@ def main():
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
 
+    kk_rct = kk_img.get_rect() # こうかとんのRectを取得
+    kk_rct.center = 300, 200   # 初期座標を設定
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
+        key_lst = pg.key.get_pressed() # 全キーの押下状態を取得
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0, 1)
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(1, 0)
+        
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(flip_bg_img, [-x + 1600, 0])
         screen.blit(bg_img, [-x + 3200, 0])
-        screen.blit(kk_img, [300, 200])
+
+        screen.blit(kk_img, kk_rct)
+
         pg.display.update()
         tmr += 1        
         clock.tick(200)
